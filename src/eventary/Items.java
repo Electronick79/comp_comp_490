3d0085600ea1754a4ee8a9f59075220f5839a038
Electronick79 <63924606+Electronick79@users.noreply.github.com>
2021-04-29 23:14:12 -0400

    Initial commit

---
+++ src/eventary/Items.java
@@ -0,0 +1,32 @@
+package eventary;
+
+public enum Items {
+    ;
+
+    public void setName(String name) {
+    }
+
+    public void setpNumber(double pNumber) {
+    }
+
+    public void setUnits(double units) {
+    }
+
+    public void setPrice(double price) {
+    }
+
+    public Object getName() {
+    }
+
+    public String getpNumber() {
+    }
+
+    public String getUnits() {
+    }
+
+    public String getPrice() {
+    }
+
+    public Object calculateTotalPrice() {
+    }
+}
